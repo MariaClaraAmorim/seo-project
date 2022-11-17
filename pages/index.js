4f26e959f76b5714cd205b70c18435e76e9179bb
maria.amorim <maria.amorim@dantaseletro.com.br>
2022-11-17 11:30:38 -0300

    feat: add next seo

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,4 @@
+import { NextSeo } from "next-seo";
 import Head from "next/head";
 import About from "./about";
 import Product from "./products/[productId]";
@@ -14,6 +15,27 @@ export default function Home() {
         <meta property="og:type" content="website" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
+
+      <NextSeo
+        title=""
+        description="Este exemplo usa mais opções de configuração disponíveis."
+        canonical="https://www.canonical.ie/"
+        openGraph={{
+          url: "https://seo-project-omega.vercel.app/",
+          title: "Open Graph Title",
+          description: "Open Graph Description",
+          images: [
+            {
+              url: "https://oggibikes.com.br/wp-content/uploads/2022/04/bw-7-4-azul-scaled.jpg",
+              width: 800,
+              height: 600,
+              alt: "Og Image Alt",
+              type: "image/jpeg",
+            },
+          ],
+          site_name: "Teste SEO",
+        }}
+      />
 
       <div>
         <h1 className="title"> Dantas Bikes</h1>
