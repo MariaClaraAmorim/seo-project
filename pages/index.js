a16de83cf3042114b157b9514f33794fb9cfd01c
maria.amorim <maria.amorim@dantaseletro.com.br>
2022-11-17 11:42:40 -0300

    feat: add host

--- pages/index.js
+++ pages/index.js
@@ -3,21 +3,15 @@ import Head from "next/head";
 import About from "./about";
 import Product from "./products/[productId]";
 
-export default function Home() {
+export default function Home({host}) {
   return (
     <>
       <Head>
-        <title>Home</title>
-        <meta x name="description" content="Home da página de teste SEO" />
-        <meta property="og:title" content="Home - Página teste" />
-        <meta property="og:description" content="Home da página de teste SEO" />
-        <meta property="og:url" content="https://myclothingstore.com/about" />
-        <meta property="og:type" content="website" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <NextSeo
-        title=""
+        title="Home"
         description="Este exemplo usa mais opções de configuração disponíveis."
         canonical="https://www.canonical.ie/"
         openGraph={{
@@ -26,7 +20,7 @@ export default function Home() {
           description: "Open Graph Description",
           images: [
             {
-              url: "https://oggibikes.com.br/wp-content/uploads/2022/04/bw-7-4-azul-scaled.jpg",
+              url:  host + "/bikeOGGI.jpg",
               width: 800,
               height: 600,
               alt: "Og Image Alt",
@@ -42,4 +36,9 @@ export default function Home() {
       </div>
     </>
   );
+}
+
+// Obtém o post completo
+export async function getStaticProps({ params }) {
+	return { props: { host: process.env["HOST"]} };
 }
