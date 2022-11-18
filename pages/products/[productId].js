f8729c8631b6955c86e37eff17c48d15590baab9
maria.amorim <maria.amorim@dantaseletro.com.br>
2022-11-18 13:51:35 -0300

    refactor: remove head

--- pages/products/[productId].js
+++ pages/products/[productId].js
@@ -2,37 +2,9 @@ import { NextSeo } from "next-seo";
 import Head from "next/head";
 import styles from "../../styles/Home.module.css";
 
-// const url = `http://localhost:3000/products/${productId}`;
-
 export default function Product({ productId, title }) {
   return (
     <>
-      <NextSeo
-        title={title}
-        description="Este exemplo usa mais opções de configuração disponíveis."
-        canonical="https://www.canonical.ie/"
-        openGraph={{
-          url: "https://seo-project-omega.vercel.app/",
-          title: "Open Graph Title",
-          description: "Open Graph Description",
-          images: [
-            {
-              url: "https://oggibikes.com.br/wp-content/uploads/2022/04/bw-7-4-azul-scaled.jpg",
-              width: 800,
-              height: 600,
-              alt: "Og Image Alt",
-              type: "image/jpeg",
-            },
-          ],
-          site_name: "Teste SEO",
-        }}
-        twitter={{
-          
-          handle: "@seo",
-          site: "@seo",
-          cardType: "summary_large_image",
-        }}
-      />
       <div className={styles.container}>
         <main className={styles.main}>
           <h1 className={styles.title}>{title}</h1>
