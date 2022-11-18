848164537482ac23d198519123893f6c52ac1b37
maria.amorim <maria.amorim@dantaseletro.com.br>
2022-11-18 14:25:55 -0300

    refactor: novas metas

--- pages/index.js
+++ pages/index.js
@@ -8,6 +8,47 @@ export default function Home() {
       <Head>
         <title>Home | Meu portfólio</title>
         <meta
+          name="description"
+          content="Sou um desenvolvedor Front-end e aqui apresento alguns projetos desenvolvidos por mim!"
+        />
+
+        {/* <meta
+          property="og:url"
+          content="https://seo-project-omega.vercel.app/"
+        /> */}
+        <meta property="og:type" content="website" />
+        <meta property="og:title" content="Home | Meu portfólio" />
+        <meta
+          property="og:description"
+          content="Sou um desenvolvedor Front-end e aqui apresento alguns projetos desenvolvidos por mim!"
+        />
+        <meta
+          property="og:image"
+          content="/bikeOGGI.png"
+        />
+
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta
+          property="twitter:domain"
+          content="seo-project-omega.vercel.app"
+        />
+        {/* <meta
+          property="twitter:url"
+          content="https://seo-project-omega.vercel.app/"
+        /> */}
+        <meta property="og:image" content="/bikeOGGI.png" />
+        <meta name="twitter:title" content="Home | Meu portfólio" />
+        <meta
+          name="twitter:description"
+          content="Sou um desenvolvedor Front-end e aqui apresento alguns projetos desenvolvidos por mim!"
+        />
+        <meta
+          name="twitter:image"
+          content="/bikeOGGI.png"
+        />
+
+        {/* <title>Home | Meu portfólio</title>
+        <meta
           name="title"
           content="Home | Meu portfólio"
         />
@@ -30,7 +71,7 @@ export default function Home() {
         <meta
           property="og:description"
           content="Sou um desenvolvedor Front-end e aqui apresento alguns projetos desenvolvidos por mim!"
-        />
+        /> */}
       </Head>
       <div>
         <h1 className="title"> Dantas Bikes</h1>
